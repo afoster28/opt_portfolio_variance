e531c503e9828792d3836369995e3489c0bf9378
Adam <afoster800@gmail.com>
2024-01-07 21:48:54 +0100

    Accurate pricer param setting and sample input change

--- opt.cpp
+++ opt.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <cmath>
 #include <random>
 
@@ -56,11 +57,38 @@ int main() {
     std::string direction = "L";
     std::string optionType = "C";
     double spotPrice = 5000.0;
-    double strikePrice = 5000.0;
+    double strikePrice = 4800.0;
     double interestRate = 0.05;
     double volatility = 0.2;
     double timeToExpiration = 60.0;
     double quantity = 10.0;
+
+    // std::cout << "Enter variables separated by commas (e.g., C, L, 4800.0, 60.0, 10.0): ";
+
+    // std::string userInput;
+    // std::getline(std::cin, userInput);
+    // std::istringstream input_stream(userInput);
+
+    // // Variables to store the parsed values
+    // std::string optionType, direction;
+    // double strikePrice, timeToExpiration, quantity;
+
+    // // Attempt to read the variables from the stream
+    // if (std::getline(input_stream, optionType, ',') &&
+    //     std::getline(input_stream, direction, ',') &&
+    //     (input_stream >> strikePrice >> std::ws && input_stream.get() == ',') &&
+    //     (input_stream >> timeToExpiration >> std::ws && input_stream.get() == ',') &&
+    //     (input_stream >> quantity)) {
+    //     // Successfully parsed the variables
+    //     std::cout << "optType: " << optionType << std::endl;
+    //     std::cout << "dir: " << direction << std::endl;
+    //     std::cout << "K: " << strikePrice << std::endl;
+    //     std::cout << "T: " << timeToExpiration << std::endl;
+    //     std::cout << "Q: " << quantity << std::endl;
+    // } else {
+    //     // Failed to parse the variables
+    //     std::cerr << "Error parsing input. Make sure to use commas to separate variables." << std::endl;
+    // }
 
     BlackScholes option(direction, optionType, spotPrice, strikePrice, interestRate, volatility, timeToExpiration, quantity);
 
